44283c0259e809dda96aa2319fbc89513faa6fd4
vishal <vishal@hadoop>
2018-09-16 15:21:36 -0700

    updated with comments

--- src/main/java/my/spring/examples/SpringMain.java
+++ src/main/java/my/spring/examples/SpringMain.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringMain {
 	public static void main(String[] args) {
 		System.out.println("Loading ApplicationContext");
-		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("application-context.xml");
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("application-context.xml"); // gets and loads the ApplicationContext file
 		ac.close();
 	}
 }
